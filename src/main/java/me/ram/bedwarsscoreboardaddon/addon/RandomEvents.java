901f0dc1a81abbc3bc64ddb7ec1d33f08d00b5f0
LinMoyuu <161136026+LinMoyuu@users.noreply.github.com>
2025-12-18 19:14:56 +0800

    修改 事件的subtitle

--- src/main/java/me/ram/bedwarsscoreboardaddon/addon/RandomEvents.java
+++ src/main/java/me/ram/bedwarsscoreboardaddon/addon/RandomEvents.java
@@ -6,19 +6,17 @@ import org.bukkit.potion.PotionEffectType;
 @Getter
 public enum RandomEvents {
 
-    PLAYERS_SPEED("全员速度提升", PotionEffectType.SPEED),
-    PLAYERS_JUMP_BOOST("全员跳跃提升", PotionEffectType.JUMP),
-    PLAYERS_STRENGTH("全员力量提升", PotionEffectType.INCREASE_DAMAGE);
+    PLAYERS_SPEED("全员速度提升", PotionEffectType.SPEED, "§c§l全体90秒速度加成!"),
+    PLAYERS_JUMP_BOOST("全员跳跃提升", PotionEffectType.JUMP, "§c§l全体90秒跳跃加成!"),
+    PLAYERS_STRENGTH("全员力量提升", PotionEffectType.INCREASE_DAMAGE, "§c§l全体90秒力量加成!");
 
     private final String eventName;
     private final PotionEffectType effectType;
+    private final String subtitle;
 
-    RandomEvents(String eventName, PotionEffectType effectType) {
+    RandomEvents(String eventName, PotionEffectType effectType, String subtitle) {
         this.eventName = eventName;
         this.effectType = effectType;
-    }
-
-    public String getSubtitle() {
-        return "§a§l" + eventName + "!";
+        this.subtitle = subtitle;
     }
 }
