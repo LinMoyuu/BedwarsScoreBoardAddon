5f29e8062efc612e9bfeef0dfd53d8b8371bd81b
LinMoyuu <161136026+LinMoyuu@users.noreply.github.com>
2025-12-19 18:43:11 +0800

    修改 随机事件 字符颜色

--- src/main/java/me/ram/bedwarsscoreboardaddon/addon/RandomEvents.java
+++ src/main/java/me/ram/bedwarsscoreboardaddon/addon/RandomEvents.java
@@ -6,9 +6,9 @@ import org.bukkit.potion.PotionEffectType;
 @Getter
 public enum RandomEvents {
 
-    PLAYERS_SPEED("全员速度提升", PotionEffectType.SPEED, "§c§l全体90秒速度加成!"),
-    PLAYERS_JUMP_BOOST("全员跳跃提升", PotionEffectType.JUMP, "§c§l全体90秒跳跃加成!"),
-    PLAYERS_STRENGTH("全员力量提升", PotionEffectType.INCREASE_DAMAGE, "§c§l全体90秒力量加成!");
+    PLAYERS_SPEED("全员速度提升", PotionEffectType.SPEED, "§4§l全体90秒速度加成!"),
+    PLAYERS_JUMP_BOOST("全员跳跃提升", PotionEffectType.JUMP, "§4§l全体90秒跳跃加成!"),
+    PLAYERS_STRENGTH("全员力量提升", PotionEffectType.INCREASE_DAMAGE, "§4§l全体90秒力量加成!");
 
     private final String eventName;
     private final PotionEffectType effectType;
